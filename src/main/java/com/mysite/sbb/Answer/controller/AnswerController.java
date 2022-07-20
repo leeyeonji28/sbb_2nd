24eae4795a981f787793a3d987295f92f173c66b
leeyeonji28 <yeon102800@gmail.com>
2022-07-20 20:47:06 +0900

    AnswerController에서 폼 데이터 처리

--- src/main/java/com/mysite/sbb/Answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/Answer/controller/AnswerController.java
@@ -1,24 +1,29 @@
 package com.mysite.sbb.Answer.controller;
 
-import com.mysite.sbb.Answer.dao.AnswerRepository;
-import com.mysite.sbb.Answer.domain.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mysite.sbb.Answer.answer.AnswerService;
+import com.mysite.sbb.Question.domain.Question;
+import com.mysite.sbb.Question.service.QuestionService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/answer")
+@AllArgsConstructor
 public class AnswerController {
 
-    @Autowired
-    private AnswerRepository answerRepository;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
 
-    @RequestMapping("/list")
-    @ResponseBody
-    public List<Answer> showAnswers(){
-        return answerRepository.findAll();
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+        Question question = this.questionService.getQuestion(id); // 질문 불러오기
+        // 질문 만들기
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail%s", id);
     }
 }
