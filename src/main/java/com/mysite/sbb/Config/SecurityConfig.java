180e73297b953b18efaf73aa382b7f747810cdd0
leeyeonji28 <yeon102800@gmail.com>
2022-07-23 22:58:44 +0900

    회원가입 구현

--- src/main/java/com/mysite/sbb/Config/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/Config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +14,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll() // 인증되지 않은 모든 요청을 허락 == 로그인하지 않더라도 접근 가능
+                .and()
+                .csrf()
+                .disable()
         ;
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/mysite/sbb/User/service/UserService.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.User.service;
+
+import com.mysite.sbb.User.dao.UserRepository;
+import com.mysite.sbb.User.domain.SiteUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        // BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // 암호화하여 비밀번호 저장
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return  user;
+    }
+}
